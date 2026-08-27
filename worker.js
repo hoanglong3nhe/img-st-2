f19e29f45b57c1722c51ed01560eb241b721b8f4
hoanglong3nhe <hoanglong.3nhe@gmail.com>
2026-08-27 20:43:52 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -11,7 +11,7 @@ export default {
     const n = slug.match(/^anh(\d+)$/);
     const i = n ? n[1] : "3";
     const uniqueId = Date.now() + Math.random().toString(36).slice(2, 9);
-    const imageUrl = `https://vidiy.fit/IMG%20(${i}).png`;
+    const imageUrl = `https://vidiy.fit/${i}.png`;
     const redirectUrl = "https://baggyrepackingrocky.com/2022576";
 
     const html = `<!DOCTYPE html>
